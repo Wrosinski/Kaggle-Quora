5e8d34342f1a0795cab192fab8fb7ea1142dc7c3
Wrosinski <wojtusior@gmail.com>
2017-05-30 15:06:54 +0200

    Update. 30.05

--- models/models_utils_gbm.py
+++ models/models_utils_gbm.py
@@ -9,7 +9,7 @@ import numpy as np
 import xgboost as xgb
 import lightgbm as lgb
 from sklearn.metrics import log_loss
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedKFold
 
 
 def predict_test(model_name):
@@ -32,56 +32,24 @@ def transform(x):
     return xt
 
 
-def run_lgb():
-    params = {
-        'task' : 'train',
-        'boosting_type' : 'gbdt',
-        'objective' : 'binary',
-        'metric' : {'binary_logloss'},
-        'learning_rate' : 0.02,
-        'feature_fraction' : 0.7,
-        'bagging_fraction': 0.9,
-        'bagging_freq': 100,
-        'num_leaves' : 255,
-        'max_depth': 12,
-        'min_data_in_leaf': 20,
-        'subsample': 0.7,
-        'colsample_bytree': 0.5,
-        'silent': 1,
-        'random_state': 1337,
-        'verbose': 1,
-        'nthread': 9,
-    }
+def run_lgb(X_train, y_train, params, name):
 
     t = time.time()
     X_tr, X_val, y_tr, y_val = train_test_split(X_train, y_train, stratify = y_train,
                                                     test_size = 0.2, random_state = 111)
     lgb_train = lgb.Dataset(X_tr, y_tr.is_duplicate.values)
     lgb_val = lgb.Dataset(X_val, y_val.is_duplicate.values)
-    print('Start training...')
+    print('Start training with parameters:', params)
     gbm = lgb.train(params, lgb_train, num_boost_round = 100000, valid_sets = lgb_val,
-                    early_stopping_rounds = 100, verbose_eval = 100)
+                    early_stopping_rounds = 250, verbose_eval = 100)
     print('Start predicting...')
     val_pred = gbm.predict(X_val, num_iteration=gbm.best_iteration)
     score = log_loss(y_val, val_pred)
     print('Final score:', score, '\n', 'Time it took to train and predict:', time.time() - t)
-    gbm.save_model('saved_models/LGBM/LGBM_500bestexperiments_loss{:.5f}.txt'.format(score))
+    gbm.save_model('saved_models/LGBM/LGBM_{}_loss{:.5f}.txt'.format(name, score))
     return gbm
 
-def run_xgb():
-    params = {
-        'seed': 1337,
-        'colsample_bytree': 0.48,
-        'silent': 1,
-        'subsample': 0.74,
-        'eta': 0.05,
-        'objective': 'binary:logistic',
-        'eval_metric': 'logloss',
-        'max_depth': 12,
-        'min_child_weight': 20,
-        'nthread': 8,
-        'tree_method': 'hist',
-        }
+def run_xgb(X_train, y_train, params, name):
 
     t = time.time()
     X_tr, X_val, y_tr, y_val = train_test_split(X_train, y_train, stratify = y_train,
@@ -89,13 +57,13 @@ def run_xgb():
     dtrain = xgb.DMatrix(X_tr, label = y_tr)
     dval = xgb.DMatrix(X_val, label = y_val)
     watchlist = [(dtrain, 'train'), (dval, 'valid')]
-    print('Start training...')
+    print('Start training with parameters:', params)
     gbm = xgb.train(params, dtrain, 100000, watchlist, 
-                    early_stopping_rounds = 100, verbose_eval = 100)
+                    early_stopping_rounds = 250, verbose_eval = 100)
     print('Start predicting...')
     train_pred = gbm.predict(xgb.DMatrix(X_tr), ntree_limit=gbm.best_ntree_limit)
     val_pred = gbm.predict(xgb.DMatrix(X_val), ntree_limit=gbm.best_ntree_limit)
     score = log_loss(y_val, val_pred)
     print('Final score:', score, '\n', 'Time it took to train and predict:', time.time() - t)
-    gbm.save_model('saved_models/XGB/XGB_500cols_furtherExperiments_{:.5f}.txt'.format(score))
+    gbm.save_model('saved_models/XGB/XGB_{}_{:.5f}.txt'.format(name, score))
     return gbm
---
+++ models/models_utils_skf.py
@@ -0,0 +1,188 @@
+import os
+import time
+import pandas as pd
+import numpy as np
+import xgboost as xgb
+import lightgbm as lgb
+import warnings
+warnings.filterwarnings('ignore')
+
+from sklearn.metrics import log_loss
+from sklearn.model_selection import train_test_split, StratifiedKFold
+
+
+def lgb_foldrun(X, y, params, name):
+    skf = StratifiedKFold(n_splits = 10, random_state = 111, shuffle = True)
+    if isinstance(X, pd.core.frame.DataFrame):
+        X = X.values
+    if isinstance(y, pd.core.frame.DataFrame):
+        y = y.is_duplicate.values
+    if isinstance(y, pd.core.frame.Series):
+        y = y.values
+    print('Running LGBM model with parameters:', params)
+        
+    i = 1
+    losses = []
+    oof_train = np.zeros((X.shape[0]))
+    os.makedirs('saved_models/LGBM/SKF/{}'.format(name), exist_ok = True)
+    for tr_index, val_index in skf.split(X, y):
+        X_tr, X_val = X[tr_index], X[val_index]
+        y_tr, y_val = y[tr_index], y[val_index]
+        t = time.time()
+        
+        lgb_train = lgb.Dataset(X_tr, y_tr)
+        lgb_val = lgb.Dataset(X_val, y_val)
+        print('Start training on fold: {}'.format(i))
+        gbm = lgb.train(params, lgb_train, num_boost_round = 100000, valid_sets = lgb_val,
+                        early_stopping_rounds = 200, verbose_eval = 100)
+        print('Start predicting...')
+        val_pred = gbm.predict(X_val, num_iteration=gbm.best_iteration)
+        oof_train[val_index] = val_pred
+        score = log_loss(y_val, val_pred)
+        losses.append(score)
+        print('Final score for fold {} :'.format(i), score, '\n',
+              'Time it took to train and predict on fold:', time.time() - t, '\n')
+        gbm.save_model('saved_models/LGBM/SKF/{}/LGBM_10SKF_loss{:.5f}_fold{}.txt'.format(name, score, i))
+        i += 1
+    np.save('OOF_preds/train/{}'.format(name), oof_train)
+    print('Mean logloss for model in 10-folds SKF:', np.array(losses).mean(axis = 0))
+    return
+
+
+def xgb_foldrun(X, y, params, name):
+    skf = StratifiedKFold(n_splits = 10, random_state = 111, shuffle = True)
+    if isinstance(X, pd.core.frame.DataFrame):
+        X = X.values
+    if isinstance(y, pd.core.frame.DataFrame):
+        y = y.is_duplicate.values
+    if isinstance(y, pd.core.frame.Series):
+        y = y.values
+    print('Running XGB model with parameters:', params)
+    
+    i = 1
+    losses = []
+    oof_train = np.zeros((X.shape[0]))
+    os.makedirs('saved_models/XGB/SKF/{}'.format(name), exist_ok = True)
+    for tr_index, val_index in skf.split(X, y):
+        X_tr, X_val = X[tr_index], X[val_index]
+        y_tr, y_val = y[tr_index], y[val_index]
+        t = time.time()
+        
+        dtrain = xgb.DMatrix(X_tr, label = y_tr)
+        dval = xgb.DMatrix(X_val, label = y_val)
+        watchlist = [(dtrain, 'train'), (dval, 'valid')]
+        print('Start training on fold: {}'.format(i))
+        gbm = xgb.train(params, dtrain, 100000, watchlist, 
+                        early_stopping_rounds = 200, verbose_eval = 100)
+        print('Start predicting...')
+        val_pred = gbm.predict(xgb.DMatrix(X_val), ntree_limit=gbm.best_ntree_limit)
+        oof_train[val_index] = val_pred
+        score = log_loss(y_val, val_pred)
+        losses.append(score)
+        print('Final score for fold {} :'.format(i), score, '\n',
+              'Time it took to train and predict on fold:', time.time() - t, '\n')
+        gbm.save_model('saved_models/XGB/SKF/{}/XGB_10SKF_loss{:.5f}_fold{}.txt'.format(name, score, i))
+        i += 1
+    np.save('OOF_preds/train/{}'.format(name), oof_train)
+    print('Mean logloss for model in 10-folds SKF:', np.array(losses).mean(axis = 0))
+    return
+
+
+
+
+def lgb_foldrun_test(X, y, X_test, params, name):
+    skf = StratifiedKFold(n_splits = 10, random_state = 111, shuffle = True)
+    if isinstance(X, pd.core.frame.DataFrame):
+        X = X.values
+    if isinstance(y, pd.core.frame.DataFrame):
+        y = y.is_duplicate.values
+    if isinstance(y, pd.core.frame.Series):
+        y = y.values
+    print('Running LGBM model with parameters:', params)
+        
+    i = 0
+    losses = []
+    oof_train = np.zeros((X.shape[0]))
+    oof_test = np.zeros((10, X_test.shape[0]))
+    os.makedirs('saved_models/LGBM/SKF/{}'.format(name), exist_ok = True)
+    for tr_index, val_index in skf.split(X, y):
+        X_tr, X_val = X[tr_index], X[val_index]
+        y_tr, y_val = y[tr_index], y[val_index]
+        t = time.time()
+        
+        lgb_train = lgb.Dataset(X_tr, y_tr)
+        lgb_val = lgb.Dataset(X_val, y_val)
+        print('Start training on fold: {}'.format(i))
+        gbm = lgb.train(params, lgb_train, num_boost_round = 100000, valid_sets = lgb_val,
+                        early_stopping_rounds = 200, verbose_eval = 100)
+        print('Start predicting...')
+        val_pred = gbm.predict(X_val, num_iteration=gbm.best_iteration)
+        oof_train[val_index] = val_pred
+        score = log_loss(y_val, val_pred)
+        losses.append(score)
+        if X_test is not None:
+            test_preds = gbm.predict(X_test)
+            oof_test[i, :] = test_preds
+        print('Final score for fold {} :'.format(i), score, '\n',
+              'Time it took to train and predict on fold:', time.time() - t, '\n')
+        gbm.save_model('saved_models/LGBM/SKF/{}/LGBM_10SKF_loss{:.5f}_fold{}.txt'.format(name, score, i))
+        i += 1
+    np.save('OOF_preds/train/{}'.format(name), oof_train)
+    print('Mean logloss for model in 10-folds SKF:', np.array(losses).mean(axis = 0))
+    oof_train = pd.DataFrame(oof_train)
+    oof_train.columns = ['{}_prob'.format(name)]
+    oof_train.to_pickle('OOF_preds/train/train_preds_{}.pkl'.format(name))
+    oof_test = oof_test.mean(axis = 0)
+    oof_test = pd.DataFrame(oof_test)
+    oof_test.columns = ['{}_prob'.format(name)]
+    oof_test.to_pickle('OOF_preds/test/test_preds_{}.pkl'.format(name))
+    return oof_test
+
+def xgb_foldrun_test(X, y, X_test, params, name):
+    skf = StratifiedKFold(n_splits = 10, random_state = 111, shuffle = True)
+    if isinstance(X, pd.core.frame.DataFrame):
+        X = X.values
+    if isinstance(y, pd.core.frame.DataFrame):
+        y = y.is_duplicate.values
+    if isinstance(y, pd.core.frame.Series):
+        y = y.values
+    print('Running XGB model with parameters:', params)
+    
+    i = 0
+    losses = []
+    oof_train = np.zeros((X.shape[0]))
+    oof_test = np.zeros((10, X_test.shape[0]))
+    os.makedirs('saved_models/XGB/SKF/{}'.format(name), exist_ok = True)
+    for tr_index, val_index in skf.split(X, y):
+        X_tr, X_val = X[tr_index], X[val_index]
+        y_tr, y_val = y[tr_index], y[val_index]
+        t = time.time()
+        
+        dtrain = xgb.DMatrix(X_tr, label = y_tr)
+        dval = xgb.DMatrix(X_val, label = y_val)
+        watchlist = [(dtrain, 'train'), (dval, 'valid')]
+        print('Start training on fold: {}'.format(i))
+        gbm = xgb.train(params, dtrain, 100000, watchlist, 
+                        early_stopping_rounds = 200, verbose_eval = 100)
+        print('Start predicting...')
+        val_pred = gbm.predict(xgb.DMatrix(X_val), ntree_limit=gbm.best_ntree_limit)
+        oof_train[val_index] = val_pred
+        score = log_loss(y_val, val_pred)
+        losses.append(score)
+        if X_test is not None:
+            test_preds = gbm.predict(X_test)
+            oof_test[i, :] = test_preds
+        print('Final score for fold {} :'.format(i), score, '\n',
+              'Time it took to train and predict on fold:', time.time() - t, '\n')
+        gbm.save_model('saved_models/XGB/SKF/{}/XGB_10SKF_loss{:.5f}_fold{}.txt'.format(name, score, i))
+        i += 1
+    np.save('OOF_preds/train/{}'.format(name), oof_train)
+    print('Mean logloss for model in 10-folds SKF:', np.array(losses).mean(axis = 0))
+    oof_train = pd.DataFrame(oof_train)
+    oof_train.columns = ['{}_prob'.format(name)]
+    oof_train.to_pickle('OOF_preds/train/train_preds_{}.pkl'.format(name))
+    oof_test = oof_test.mean(axis = 0)
+    oof_test = pd.DataFrame(oof_test)
+    oof_test.columns = ['{}_prob'.format(name)]
+    oof_test.to_pickle('OOF_preds/test/test_preds_{}.pkl'.format(name))
+    return oof_test
